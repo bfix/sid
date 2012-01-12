8ecf11af5f178d727d06b6973e4a395ea644143d
Bernd Fix <bernd.fix@aspector.com>
2012-01-12 21:19:48 +0100

    Find executable for different build methods (make, gb, ...)

--- runtime/run.sh
+++ runtime/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 cd runtime
-../src/sid
+clear
+
+EXEC=""
+[ -x ../bin/src ] && EXEC=../bin/src
+[ -x ../src/sid ] && EXEC=../src/sid
+
+echo "Running '${EXEC}'..."
+
+${EXEC}
 
 exit 0
