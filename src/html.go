f840b4d1452bed3a5e82e2b6afc3d1b9d4ec8c41
Bernd Fix <bernd.fix@aspector.com>
2012-01-14 21:24:44 +0100

    Parsing of tags for translation completed (still rudimentary).

--- src/html.go
+++ src/html.go
@@ -24,6 +24,8 @@ package main
 
 import (
 	"bufio"
+	"html"
+	"gospel/logger"
 )
 
 ///////////////////////////////////////////////////////////////////////
@@ -35,26 +37,99 @@ import (
  * The parser builds a list of inline ressources referenced in the HTML file;
  * these are the ressources the client browser will request when loading the
  * HTML page, so that it behaves like a genuine access if monitored by an
- * eavesdropper.
+ * eavesdropper. This function adds to an existing list of ressources (from
+ * a previous cover server response).
  * @param rdr *bufio.Reader - buffered reader for parsing
- * @return []string - list of inline ressources
+ * @param list map[string]string - (current) list of inline ressources
  */
-func parseHTML (rdr *bufio.Reader) []string {
-	list := make ([]string, 0)
+func parseHTML (rdr *bufio.Reader, list []*Tag) {
+	
+	// try to use GO html tokenizer to parse the content
+	tk := html.NewTokenizer (rdr)
+	for {
+		// get next HTML tag
+		toktype := tk.Next()
+		if toktype == html.ErrorToken {
+			// parsing error: most probable case is that the tag spans
+			// fragments. This is only a problem if it concerns a tag
+			// for possible translation (currently unhandled)
+			logger.Println (logger.ERROR, "[html] Error parsing content")
+			break
+		}
+		if toktype == html.StartTagToken || toktype == html.SelfClosingTagToken {
+			// we are only interessted in certain tags
+			tag,_ := tk.TagName()
+			name := string(tag)
+			switch name {
+				//-----------------------------------------------------
+				// external script files
+				//-----------------------------------------------------
+				case "script":
+					attrs := getAttrs (tk)
+					if _,ok := attrs["src"]; ok {
+						// add external reference to script file
+						t := NewTag ("script", attrs)
+						list = append (list, t)
+						logger.Println (logger.DBG, "[html] => " + t.String())
+					}
+
+				//-----------------------------------------------------
+				// external image
+				//-----------------------------------------------------
+				case "img":
+					attrs := getAttrs (tk)
+					t := NewTag ("img", attrs)
+					list = append (list, t)
+					logger.Println (logger.DBG, "[html] => " + t.String())
+					
+				//-----------------------------------------------------
+				// unknown
+				//-----------------------------------------------------
+				default:
+					logger.Println (logger.DBG_ALL, "*** " + name)
+			}
+		}
+	}
+}
+
+//---------------------------------------------------------------------
+/*
+ * Get list of attributes for a tag
+ * @param tk *html.Tokenizer - tokenizer instance
+ * @return map[string]string - list of attributes
+ */
+func getAttrs (tk *html.Tokenizer) map[string]string {
+	list := make (map[string]string)			
+	for {
+		key,val,more := tk.TagAttr()
+		list[string(key)] = string(val)
+		if !more {
+			break
+		}	
+	}
 	return list
 }
 
 //---------------------------------------------------------------------
 /*
- * Assemble a response from the current state (like response header),
- * the resource list and a replacement body (addressed by the requested
- * ressource path from state).
- * @param s *state - current state info
- * @param resList []string - list of ressources to be included in response
- * @param size int - target size of response
- * @return []byte - assembled response
+ * Generate HTML padding sequence.
+ * @param size int - length of padding sequence
+ * @return string - padding sequence 
  */
-func (c *Cover) assembleHTML (s *state, resList []string, size int) []byte {
-	resp := make ([]byte, 0)
-	return resp
+func padding (size int) string {
+	// small paddings are simple spaces...
+	if size < 9 {
+		s := ""
+		for n := 0; n < size; n++ {
+			s += " "
+		}
+		return s
+	}
+	// lomger paddings are wrapped into a HTML comment
+	size -= 9
+	s := "<!-- "
+	for n := 0; n < size; n++ {
+		s += "?"
+	}
+	return s + " -->"
 }
