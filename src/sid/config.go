825c619bed23002ca6e5811d78ced2952ba29d8f
Bernd Fix <bernd.fix@aspector.com>
2012-04-11 13:53:57 +0200

    Set network communication parameters (delay, retries and timeouts) in configuration.

--- src/sid/config.go
+++ src/sid/config.go
@@ -28,8 +28,10 @@ import (
 	"flag"
 	"gospel/logger"
 	"gospel/parser"
+	"gospel/network"
 	"os"
 	"strconv"
+	"time"
 )
 
 ///////////////////////////////////////////////////////////////////////
@@ -144,6 +146,11 @@ func InitConfig() {
 			CfgData.LogState = false
 		}
 	}
+	
+	// set networking parameter
+	network.Delay   = 1000000 // 1ms
+	network.Retries = 1000    // max. 1s
+	network.Timeout,_ = time.ParseDuration("100us") // 0.1ms
 
 	// list current configuration data
 	logger.Println(logger.INFO, "[sid.config] !==========< configuration >===============")
